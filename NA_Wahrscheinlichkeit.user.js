b9d03579902e81cef04d0b346088a3e46628b889
TutePlays <tutedutsch@gmail.com>
2019-05-25 16:47:07 +0200

    Update NA_Wahrscheinlichkeit.user.js

--- NA_Wahrscheinlichkeit.user.js
+++ NA_Wahrscheinlichkeit.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Zeige NA Wahrscheinlichkeit
 // @namespace    https://youtube.com/tuteplays
-// @version      0.1
+// @version      1.0
 // @description  Zeigt neben der Anzahl verletzter Personen in der Einsatzmaske die Wahrscheinlichkeit für einen Notarzt.
-// @author       TutePlays
+// @author       TutePlays (API von Jan (KBOE2)/LSS-Manager)
 // @match        https://www.leitstellenspiel.de/missions/*
 // @grant        none
 // ==/UserScript==
@@ -13,14 +13,26 @@
     'use strict';
 
     function NA_Wahrscheinlichkeit() {
-        var MissionHelper = $("#mission-form .alert.alert-warning:contains('Notarzteinsatzfahrzeug')");
-        var NA_Wahrscheinlichkeit = MissionHelper.text().substr(MissionHelper.text().indexOf("Notarzteinsatzfahrzeug (") + 24, 3);
+        
+        var missionID = $("#mission_help").attr("href").substr(11,$("#mission_help").attr("href").indexOf("?mission_id")-11);
 
-        //Nur anzeigen, wenn NA benötigt
-        if (NA_Wahrscheinlichkeit){
-            var insertPos = $( "#missionH1" ).next().next();
-            insertPos.html(insertPos.html() + " | " + NA_Wahrscheinlichkeit + " NA ");
-        }
+        //GET Data from Jan's API
+        $.getJSON("https://lssm.ledbrain.de/api/missions.php?mission=" + missionID, function(data) {
+            var NA_Wahrscheinlichkeit = "undefined";
+            if (data.onlyRd == true){
+                NA_Wahrscheinlichkeit = data.nef;
+            } else {
+                NA_Wahrscheinlichkeit = data.patients.nef;
+            }
+
+            //Nur anzeigen, wenn NA benötigt
+            if (NA_Wahrscheinlichkeit != undefined){
+
+                var insertPos = $( "#missionH1" ).next().next();
+                insertPos.html(insertPos.html() + " | " + NA_Wahrscheinlichkeit + "% NA ");
+            }
+        });
+
 
     }
     $(window).on("load", NA_Wahrscheinlichkeit);
