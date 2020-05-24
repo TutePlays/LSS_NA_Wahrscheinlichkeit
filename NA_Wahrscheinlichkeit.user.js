3626b199ab9c49a6c06ee7bf6155030b8688f8b0
TutePlays <tutedutsch@gmail.com>
2020-05-24 21:52:35 +0200

    updated for new api

--- NA_Wahrscheinlichkeit.user.js
+++ NA_Wahrscheinlichkeit.user.js
@@ -1,40 +1,37 @@
 // ==UserScript==
 // @name         Zeige NA Wahrscheinlichkeit
 // @namespace    https://youtube.com/tuteplays
-// @version      1.0
+// @version      1.2
 // @description  Zeigt neben der Anzahl verletzter Personen in der Einsatzmaske die Wahrscheinlichkeit für einen Notarzt.
-// @author       TutePlays (API von Jan (KBOE2)/LSS-Manager)
+// @author       TutePlays, Anpassungen von Jan und Lennard
 // @match        https://www.leitstellenspiel.de/missions/*
 // @grant        none
+// @updateURL    https://github.com/TutePlays/LSS_NA_Wahrscheinlichkeit/raw/master/NA_Wahrscheinlichkeit.user.js
+// @downloadURL  https://github.com/TutePlays/LSS_NA_Wahrscheinlichkeit/raw/master/NA_Wahrscheinlichkeit.user.js
 // ==/UserScript==
-
 (function() {
-
     'use strict';
+    const missionID = $("#mission_help").attr('href').split("/").pop().replace(/\?.*/, '');
+    let mission_specs_cache = {};
 
-    function NA_Wahrscheinlichkeit() {
-        
-        var missionID = $("#mission_help").attr("href").substr(11,$("#mission_help").attr("href").indexOf("?mission_id")-11);
-
-        //GET Data from Jan's API
-        $.getJSON("https://lssm.ledbrain.de/api/missions.php?mission=" + missionID, function(data) {
-            var NA_Wahrscheinlichkeit = "undefined";
-            if (data.onlyRd == true){
-                NA_Wahrscheinlichkeit = data.nef;
-            } else {
-                NA_Wahrscheinlichkeit = data.patients.nef;
-            }
-
-            //Nur anzeigen, wenn NA benötigt
-            if (NA_Wahrscheinlichkeit != undefined){
-
-                var insertPos = $( "#missionH1" ).next().next();
-                insertPos.html(insertPos.html() + " | " + NA_Wahrscheinlichkeit + "% NA ");
-            }
-        });
-
-
+    async function output()
+    {
+        console.log("OUT!");
+        let NA_Wahrscheinlichkeit = mission_specs_cache.filter(e => e.id == missionID)[0].chances.nef;
+        NA_Wahrscheinlichkeit && $('.mission_header_info .col-md-6:first-of-type>small').append(` | ${NA_Wahrscheinlichkeit}% NA (${Math.ceil(NA_Wahrscheinlichkeit * $('.mission_patient').length / 100)})`);
     }
-    $(window).on("load", NA_Wahrscheinlichkeit);
-})();
 
+    async function init()
+    {
+        if(!sessionStorage.getItem("mission_specs_cache")){
+            await $.getJSON(`https://www.leitstellenspiel.de/einsaetze.json`, data => {
+                mission_specs_cache = data;
+                sessionStorage.setItem("mission_specs_cache", JSON.stringify(data));
+            });
+        }else{
+            mission_specs_cache = JSON.parse(sessionStorage.getItem("mission_specs_cache"));
+        }
+        output();
+    }
+    init();
+})();
